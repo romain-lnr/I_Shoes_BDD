4146f56f62fe6cae5c2a616ed6eac6235f173443
Romain-len <89740581+romain-lnr@users.noreply.github.com>
2023-06-08 20:10:27 +0200

    Put the footer in the layout page

--- src/views/site/basket.php
+++ src/views/site/basket.php
@@ -55,37 +55,3 @@ $title="IShoes - basket page"
         <a href="index.php?action=home"><input type="submit" name="insert" id="insert" value="revenir sur la page d'accueil"></a>
     </div>
     <?php } ?>
-        <br>
-          <footer>
-        <div id="contrainer">
-            <div class="row">
-                <div class="col-sm-3">
-                    <div id="footer_proj">
-                        <h3>Notre projet</h3>
-                        <p>Notre projet se base sur un site e-commerce de vente de chaussures. L'équipe est composé d'un web master, d'un responsable développeur et de quelqu'un servant à la création de maquettes et des élements ments basiques du projet.
-                            Le projet a été publié le 6 avril 2023 et a commencé quelques mois auparavant.</p>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div id="footer_desc">
-                        <h3>Descriptif du projet</h3>
-                        <p>Ce projet est, comme dit, un avant-goût d'un site e-commerce où le concept est la vente de chaussures. Vous trouverez ici nos articles les plus fameux ainsi que les derniers ajouts de nos services.
-                            Comme par exemple le principe de pouvoir se logger en tant qu'utilisateur du site, de pouvoir y regarder ces différents articles et de pouvoir ajouter un article sur votre panier d'achat afin de le payer. Vous pouvez ainsi profiter des dernières actualités en cours.
-                            Le projet a également l'éloge d'avoir un compte administrateur afin que vous pouviez quotidiennement avoir de nouveaux stocks sur vos articles préférés
-                        </p>
-                    </div>
-                </div>
-                <div class="col-sm-3">
-                    <div id="footer_contact">
-                        <h3>Contact du service</h3>
-                        <h6>Téléphone :</h6>
-                        <p>+41 67 827 65 24</p>
-                        <h6>Email :</h6>
-                        <p><a href="mailto:i_shoes.contact@gmail.com">i_shoes.contact@gmail.com</a></p>
-                        <h6>Adresse :</h6>
-                        <p>Rue de la Réunion 14<br>1450 Ste-Croix, Vaud Suisse</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
